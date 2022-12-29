3fa95cff15fb8a6d2e60cc9d3e9e7fc187db4452
Luca Alfino <alfinoluca21@gmail.com>
2022-12-29 17:28:00 +0100

    Modified table country,created table occupataion and working on patient

--- Database.sql
+++ Database.sql
@@ -2,8 +2,27 @@
 
 CREATE TABLE Country
 (
-	countryId INTEGER PRIMARY KEY,
+	countryId INTEGER CONSTRAINT cn_cid_pk PRIMARY KEY,
 	countryName NVARCHAR(100) NOT NULL,
 	riskOfForeignDisease BIT NOT NULL
 );
 GO
+
+--To show the current occupation of a patient
+CREATE TABLE Occupation
+(
+	occupationId INTEGER CONSTRAINT occ_ocid_pk PRIMARY KEY,
+	occupationName NVARCHAR(100) NOT NULL
+);
+GO
+
+CREATE TABLE Patient
+(
+	pId INTEGER CONSTRAINT pt_pid_pk PRIMARY KEY,
+	idCard NVARCHAR(10) CONSTRAINT pt_idc_un UNIQUE,
+	firstName NVARCHAR(100) NOT NULL,
+	lastName NVARCHAR(100) NOT NULL,
+	
+);
+GO
+
