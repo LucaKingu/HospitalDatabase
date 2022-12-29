8843bfe813d1f66dcc044840501897ffd2594a8b
Luca Alfino <alfinoluca21@gmail.com>
2022-12-29 17:56:18 +0100

    Created tables City,Department and Hospital_Employee

--- Database.sql
+++ Database.sql
@@ -8,6 +8,13 @@ CREATE TABLE Country
 );
 GO
 
+CREATE TABLE City
+(
+	cityId INTEGER CONSTRAINT ct_ctid_pk PRIMARY KEY,
+	cityName NVARCHAR(100) NOT NULL
+);
+GO
+
 --To show the current occupation of a patient
 CREATE TABLE Occupation
 (
@@ -29,9 +36,34 @@ CREATE TABLE Patient
 	registrationDate DATETIME NOT NULL,
 	sex BIT,
 	occupationId INTEGER NOT NULL CONSTRAINT pt_occid_fk REFERENCES Occupation(occupationId),
-	countryId INTEGER NOT NULL CONSTRAINT cn_cid_fk REFERENCES Country(countryId)
+	countryId INTEGER NOT NULL CONSTRAINT cn_cid_fk REFERENCES Country(countryId),
+	cityId INTEGER NOT NULL CONSTRAINT ct_ctid_fk REFERENCES City(cityId)
 );
 GO
 
+CREATE TABLE Department
+(
+	departmentId INTEGER CONSTRAINT dp_dpid_pk PRIMARY KEY,
+	departmentName NVARCHAR(100) NOT NULL
+);
+GO
+
+CREATE TABLE Hospital_Employee
+(
+	employeeId INTEGER CONSTRAINT emp_empid_pk PRIMARY KEY,
+	firstName NVARCHAR(100) NOT NULL,
+	lastName NVARCHAR(100) NOT NULL,
+	[address] NVARCHAR(250) NOT NULL,
+	martialStatus NVARCHAR(20) NOT NULL,
+	age INTEGER NOT NULL,
+	phone INTEGER NOT NULL,
+	hireDate DATETIME NOT NULL,
+	attritionDate DATETIME,
+	sex BIT,
+	countryId INTEGER NOT NULL CONSTRAINT cnHE_cid_fk REFERENCES Country(countryId),
+	cityId INTEGER NOT NULL CONSTRAINT ctHE_ctid_fk REFERENCES City(cityId),
+	departmentId INTEGER NOT NULL CONSTRAINT dp_dpid_fk REFERENCES department(departmentId)
+);
+GO
 
 
